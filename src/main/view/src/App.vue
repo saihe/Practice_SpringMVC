82e9f367c79debff650ad9dde45e6d591a3ab52c
Kyohei Saito <saihe.kyon@gmail.com>
2021-03-07 00:56:52 +0900

    vuetifyを追加して、画面レイアウトの叩き台作成

--- src/main/view/src/App.vue
+++ src/main/view/src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app clipped-left>
+      <v-toolbar-title>Vuetify</v-toolbar-title>
+    </v-app-bar>
+    <v-navigation-drawer app clipped>
+      <v-expansion-panels multiple>
+        <v-expansion-panel>
+          <v-expansion-panel-header>商品管理</v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-list>
+              <v-list-item>
+                <v-btn text>在庫管理</v-btn>
+              </v-list-item>
+              <v-list-item>
+                <v-btn text>商品マスタ管理</v-btn>
+              </v-list-item>
+            </v-list>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+        <v-expansion-panel>
+          <v-expansion-panel-header>アカウント管理</v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-list>
+              <v-list-item>アカウント追加</v-list-item>
+            </v-list>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-navigation-drawer>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
